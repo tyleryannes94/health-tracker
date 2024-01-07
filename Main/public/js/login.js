40e21879a1630e23c95e5d42028b0e7bcdf04f95
Suzet Salinas <suzet.sal@gmail.com>
2024-01-07 17:50:55 -0600

    moved signupformHandler struct into signup.js file; added script src tags in the login.hbs and signup.hbs files for their respective js files

--- Main/public/js/login.js
+++ Main/public/js/login.js
@@ -22,38 +22,6 @@ const loginFormHandler = async (event) => {
     }
     };
 
-
-const signupFormHandler = async (event) => {
-    event.preventDefault(); 
-  
-    // collect info from sign up form
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-    const healthGoal = document.querySelector('#health-goal-signup').value.trim();
-    const currentWeight = document.querySelector('#current-weight-signup').value.trim();
-  
-    if (name && email && password) {
-        // send POST request to API endpoint
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, email, password, healthGoal, currentWeight }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        // if successful, redirect browser to profile page
-        document.location.replace('/profile'); // TODO: change '/profile' to '/profile.js' ?
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
-
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
-
-document
-  .querySelector('.signup-form') 
-  .addEventListener('submit', signupFormHandler);
